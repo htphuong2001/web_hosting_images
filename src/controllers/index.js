51f720aa35c0de51c3f98d23e3926c252d73489e
PHUONG <htphuong200100@gmail.com>
2022-01-06 14:15:44 +0700

    login and register page

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,17 @@
+const getHomePage = async (req, res, next) => {
+  res.render("pages/home");
+};
+
 const getRegisterPage = async (req, res, next) => {
   res.render("pages/register");
 };
 
+const getLoginPage = async (req, res, next) => {
+  res.render("pages/login");
+};
+
 module.exports = {
   getRegisterPage,
+  getHomePage,
+  getLoginPage,
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
-const { getRegisterPage } = require("../controllers/index");
+const {
+  getRegisterPage,
+  getHomePage,
+  getLoginPage,
+} = require("../controllers/index");
 
 const router = express.Router();
 
+router.get("/", getHomePage);
 router.get("/register", getRegisterPage);
+router.get("/login", getLoginPage);
 
 module.exports = router;
